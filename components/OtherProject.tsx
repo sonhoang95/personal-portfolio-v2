a8b9061d0f9ac09d1c6208cf6928596c65f60c67
Son Nguyen <sonhoang95@gmail.com>
2022-03-03 12:16:40 -0600

    add more projects

--- components/OtherProject.tsx
+++ components/OtherProject.tsx
@@ -1,32 +1,49 @@
 import { ProjectData } from '../types';
 import { AiOutlineFolder } from 'react-icons/ai';
-import { FaShare } from 'react-icons/fa';
+import { FaShare, FaGithub } from 'react-icons/fa';
 import TechStack from './TechStack';
 import Link from 'next/link';
-const OtherProject = ({ name, shortDesc, siteUrl, techStack }: ProjectData) => {
-  console.log(name);
-
+const OtherProject = ({
+  name,
+  shortDesc,
+  siteUrl,
+  techStack,
+  githubUrl,
+}: ProjectData) => {
   return (
-    <article className="bg-[#112240] px-7 py-8">
-      <div className="flex items-center justify-between mb-8">
-        <span className="text-5xl text-teal-300">
-          <AiOutlineFolder />
-        </span>
-        <Link href={siteUrl}>
-          <a
-            className='className="text-xl text-slate'
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FaShare />
-          </a>
-        </Link>
-      </div>
-      <div className="space-y-4">
+    <article className="bg-[#112240] px-7 py-8 flex flex-col justify-between transform transition duration-200 ease-in-out hover:-translate-y-2">
+      <div>
+        <div className="flex items-center justify-between mb-8">
+          <span className="text-5xl text-teal-300">
+            <AiOutlineFolder />
+          </span>
+          <div className="flex items-center gap-3">
+            {githubUrl && (
+              <Link href={githubUrl}>
+                <a
+                  className="text-xl text-slate hover:text-teal-300"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FaGithub />
+                </a>
+              </Link>
+            )}
+            <Link href={siteUrl}>
+              <a
+                className="text-xl text-slate hover:text-teal-300"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaShare />
+              </a>
+            </Link>
+          </div>
+        </div>
         <h3 className="text-xl font-semibold text-[#ccd6f6]">{name}</h3>
-        <p className="text-slate text-sm leading-relaxed">{shortDesc}</p>
-        <TechStack techStack={techStack} isOtherProject={true} />
+        <p className="text-slate text-sm leading-relaxed py-4">{shortDesc}</p>
       </div>
+      <TechStack techStack={techStack} isOtherProject={true} />
     </article>
   );
 };
--- components/OtherProjects.tsx
+++ components/OtherProjects.tsx
@@ -1,12 +1,18 @@
+import { motion } from 'framer-motion';
 import { ProjectData } from '../types';
 import OtherProject from './OtherProject';
 
 const OtherProjects = ({ projects }: { projects: ProjectData[] }) => {
   const tempProjects = projects.filter(project => project.featured === false);
-  console.log(tempProjects);
 
   return (
-    <section className="max-w-[1000px] mx-auto px-6 lg:px-0">
+    <motion.section
+      className="max-w-[1000px] mx-auto px-6 lg:px-0 mb-12"
+      initial={{ opacity: 0, y: 150 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
+      transition={{ duration: 1 }}
+    >
       <h3 className="text-2xl lg:text-3xl font-semibold text-[#ccd6f6] capitalize text-center mb-12">
         Other noteworthy projects
       </h3>
@@ -15,7 +21,7 @@ const OtherProjects = ({ projects }: { projects: ProjectData[] }) => {
           <OtherProject key={index} {...project} />
         ))}
       </div>
-    </section>
+    </motion.section>
   );
 };
 
